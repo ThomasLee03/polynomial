54ca6c19d321c55063c0d20de153d6d742008d28
ThomasLee03 <125076120+ThomasLee03@users.noreply.github.com>
2024-09-11 17:29:49 -0400

    Update Polynomial.py

--- Polynomial.py
+++ Polynomial.py
@@ -1,17 +1,24 @@
-
 class X:
     def __init__(self):
         pass
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x_value):
+        # Returns the value of X, which is the variable itself
+        return x_value
 
 class Int:
     def __init__(self, i):
-        return Int(i)
+        self.i = i  # Ensure you store the value properly in self.i
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x_value):
+        # An integer evaluates to itself, independent of X
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -20,6 +27,10 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x_value):
+        # Evaluate both parts and add their values
+        return self.p1.evaluate(x_value) + self.p2.evaluate(x_value)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -29,12 +40,19 @@ class Mul:
     def __repr__(self):
         if isinstance(self.p1, Add):
             if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+                return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, x_value):
+        # Evaluate both parts and multiply their values
+        return self.p1.evaluate(x_value) * self.p2.evaluate(x_value)
 
+# Example polynomial:
+poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+# Print the polynomial and evaluate it for X = -1
 print(poly)
+print(poly.evaluate(-1))  # This should evaluate the polynomial when X = -1
